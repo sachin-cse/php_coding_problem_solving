cf1b50910b9feb699256a36e2008e8acb7f31ece
sachin <mandalsachin725@gmail.com>
2024-04-29 20:34:24 +0530

    solved

---
+++ area_of_rectangle.php
@@ -0,0 +1,7 @@
+<?php
+function area_of_rectangle($h, $w){
+    return $h*$w.'A<sup>2</sup>';
+}
+
+echo area_of_rectangle(5, 2);
+?>
---
+++ check_leap_year.php
@@ -0,0 +1,14 @@
+<?php
+function checkLeapYear($year){
+    if($year % 100 == 0 && $year % 400 == 0){
+        return $year." is a leap year";
+    }
+    else if($year%4 == 0){
+        return $year." is a leap year";
+    } else {
+        return $year." is not a leap year";
+    }
+}
+
+echo checkLeapYear(2001);
+?>
