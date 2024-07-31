5c822d7517a8dfcdd62a9d476418abe701c53891
sachin <mandalsachin725@gmail.com>
2024-07-31 21:00:23 +0530

    solved

--- reverse_string.php
+++ reverse_string.php
@@ -1,8 +1,16 @@
 <?php
-$str = "sachin";
 
-// echo strrev($str);
-for($i=strlen($str)-1; $i>=0; $i--){
-    echo $str[$i];
+function reverseString($str){
+    if($str == ""){
+        return "\"\"";
+    }
+    $rev_str = "";
+    for($i=strlen($str)-1; $i>=0; $i--){
+        $rev_str.=$str[$i];
+    }
+
+    return $rev_str;
 }
+
+echo reverseString("");
 ?>
